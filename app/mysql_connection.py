910be1201c00e4ce19069a8c86154181179f7ab8
Manpreet_Singh <manpreet01@outlook.com>
2017-02-08 14:23:02 -0800

    python,mysql and flask docker compose

---
+++ app/mysql_connection.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python
+config ={
+  'user':'root',
+  'password':'root',
+  'host': 'mysql'
+}
+
+database_name= 'malicious'
+TB_name= 'URLlookup'
+
+
+
---
+++ db/connector.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+git clone https://github.com/mysql/mysql-connector-python.git &&
+cd mysql-connector-python 
+python ./setup.py build 
+python ./setup.py install
+
