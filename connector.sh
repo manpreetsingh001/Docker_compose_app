86c8b50fe8d428f78373001c0ebdab0d3c7d59df
Manpreet_Singh <manpreet01@outlook.com>
2017-02-08 14:29:58 -0800

    files resorganized

--- connector.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/bash
-git clone https://github.com/mysql/mysql-connector-python.git &&
-cd mysql-connector-python 
-python ./setup.py build 
-python ./setup.py install
-
--- python_exe.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/bash
-python database_update.py
-sleep 10
-python URL_looup.py
-
-
